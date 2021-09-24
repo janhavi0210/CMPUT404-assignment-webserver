c4906d3b89a37a8ba3f676bf027644764f77d123
janhavi0210 <75977053+janhavi0210@users.noreply.github.com>
2021-09-24 15:35:44 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
-
+import socket
+import mimetypes
+import re
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +23,7 @@ import socketserver
 # Foundation; All Rights Reserved
 #
 # http://docs.python.org/2/library/socketserver.html
+# Path extraction code (line 38-42): https://stackoverflow.com/questions/35555427/how-to-extract-url-from-get-http-request
 #
 # run: python freetests.py
 
@@ -28,11 +31,41 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        self.data = self.request.recv(1024).strip().decode()
+
+        pattern = re.compile("^GET (.*)[ ].*")
+        try:
+            #get path from GET request
+            line=self.data
+            path = pattern.match(line).group(1)
+            print ("Got a request of: %s\n" % self.data)
+        except Exception as e:
+            #request cannot be handled
+            response = 'HTTP/1.0 305 Method Not Allowed \n\n'
+            path=""
+            self.request.sendall(response.encode())
+            return
+
+        if(path is "/"):
+            path="/index.html"
+        path="www"+path
+        print(path)
+        type=mimetypes.guess_type(path, strict=True)
+        print(type)
+        #send response
+        try:
+            fin = open(path)
+            content = fin.read()
+            fin.close()
+            response = 'HTTP/1.0 200 OK\r\n'+'Content-Type: '+type[0]+'\r\n\r\n' + content
+            self.request.sendall(response.encode())
+        except FileNotFoundError:
+            response = 'HTTP/1.0 404 NOT FOUND\n\nFile Not Found'
+            self.request.sendall(response.encode())
+            return
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -41,6 +74,6 @@ if __name__ == "__main__":
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
+# Close socket
+
     server.serve_forever()
